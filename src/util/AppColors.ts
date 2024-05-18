060252cc2ae24bff3cfd1c9ab6cbe848025a1da9
Rishabh Jain <rishabh.jain@salesfine.co>
2024-05-18 13:15:19 +0530

    Added color file

---
+++ src/util/AppColors.ts
@@ -0,0 +1,8 @@
+export const AppColors = {
+    primary:'#C60A0A',
+    background:'#090808',
+    onBackground:'white',
+    onPrimary:'white',
+    secondary:'#433C3C',
+    onSecondary:'white',
+};
